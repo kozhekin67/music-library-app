4293adf8d5737bf0ea23d3a48ec972b5d3f8cd95
KozhhekinAN <kozhekin67@gmail.com>
2025-03-02 21:32:13 +0300

    initial React App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/globals.scss';
+import './styles/reset.scss';
+
+import s from './App.module.scss';
+
+function App() {
+  return (
+    <div className={s.app}>
+      <h1>Music Libery App</h1>
+    </div>
+  );
+}
+
+export default App;
