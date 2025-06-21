ccbb339c110a6aee36c4644ec19cfaef2ee23068
KozhhekinAN <kozhekin67@gmail.com>
2025-06-21 21:10:18 +0300

    configuring the router during filtering

--- frontend/src/components/common/Filter/Filter.jsx
+++ frontend/src/components/common/Filter/Filter.jsx
@@ -1,4 +1,6 @@
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import {
   setTextFilter,
   selectTextFilter,
@@ -9,11 +11,21 @@ import Input from '../Input/Input';
 import s from './Filter.module.scss';
 
 const Filter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchQuery = searchParams.get('search') || '';
+
   const dispatch = useDispatch();
   const textFilter = useSelector(selectTextFilter);
   const handleTextFilterChange = (event) => {
-    dispatch(setTextFilter(event.target.value));
+    const newValue = event.target.value;
+    dispatch(setTextFilter(newValue));
+    setSearchParams({ search: newValue });
   };
+
+  useEffect(() => {
+    dispatch(setTextFilter(searchQuery));
+  }, [searchQuery, dispatch]);
 
   return (
     <div className={s.block}>
